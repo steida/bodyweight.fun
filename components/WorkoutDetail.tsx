a31bb0ec03c89c1480ae2b761cde9f9bcb4d0820
Daniel Steigerwald <daniel@steigerwald.cz>
2021-11-03 18:54:08 +0100

    Add the delete button to a workout detail

--- components/WorkoutDetail.tsx
+++ components/WorkoutDetail.tsx
@@ -1,11 +1,30 @@
 import { option, readonlyArray } from 'fp-ts';
 import { pipe } from 'fp-ts/function';
 import { memo, useEffect } from 'react';
+import { useIntl } from 'react-intl';
+import { View } from 'react-native';
 import { NanoID } from '../codecs/branded';
-import { useAppState } from '../contexts/AppStateContext';
+import { useAppDispatch, useAppState } from '../contexts/AppStateContext';
 import { useTheme } from '../contexts/ThemeContext';
+import { OutlineButton } from './buttons/OutlineButton';
 import { Heading } from './elements/Heading';
 import { Modal } from './Modal';
+
+const DeleteButton = ({ id }: { id: NanoID }) => {
+  const intl = useIntl();
+  const appDispatch = useAppDispatch();
+
+  const handlePress = () => {
+    appDispatch({ type: 'deleteWorkout', id });
+  };
+
+  return (
+    <OutlineButton
+      title={intl.formatMessage({ defaultMessage: 'Delete' })}
+      onPress={handlePress}
+    />
+  );
+};
 
 export const WorkoutDetail = memo<{
   id: NanoID;
@@ -31,6 +50,9 @@ export const WorkoutDetail = memo<{
         <Heading level={2} style={[t.text, t.color]}>
           {workout.name}
         </Heading>
+        <View style={[t.flexRow, t.justifyEvenly, t.mt]}>
+          <DeleteButton id={id} />
+        </View>
       </Modal>
     )
   );

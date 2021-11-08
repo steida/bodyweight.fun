60611ea536a07dc676cbcf0c8a4a1a0c4eb161f5
Daniel Steigerwald <daniel@steigerwald.cz>
2021-11-08 23:29:49 +0100

    Remove dev comments

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,14 +5,8 @@ import { useTheme } from '../contexts/ThemeContext';
 export const Layout: FC = ({ children }) => {
   const t = useTheme();
 
-  // SafeAreaView is for Safari < 15.
-  // With Safari 15, it makes no difference and it seems it's not
-  // required except home full-screen mode (a web link as icon on
-  // home screen), but it makes sense a full-screen without any safe area.
-
   return (
     <SafeAreaView>
-      {/* I don't think we need vertical padding. */}
       <View style={t.ph}>{children}</View>
     </SafeAreaView>
   );

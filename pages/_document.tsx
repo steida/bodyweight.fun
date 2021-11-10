4cb969521879b5103fbc030b2552c2a4d9a7ca96
Daniel Steigerwald <daniel@steigerwald.cz>
2021-11-10 20:50:17 +0100

    Remove dev comment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,16 +23,6 @@ export default class MyDocument extends Document {
     return { ...page, styles: Children.toArray(styles) };
   }
 
-  // RNfW documentation recommends height: '100%' to mimic native
-  // device, but it requires ScrollView with flex: 1 which:
-  // 1) Makes desktop scroll animation ugly in Chrome.
-  // 2) Breaks Safari 15 rendering via wrong body height which
-  //    makes screen cropped by navigation bar sometimes.
-  //    It's well described problem for Safari < 15 and still
-  //    buggy with Safari 15.
-  // That's why can't use height: '100%'.
-  // https://twitter.com/estejs/status/1455272407179177986
-
   render() {
     return (
       <Html lang="en">

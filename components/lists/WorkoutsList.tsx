0ac5f8512b7779cb15d4ec18a3b98732dd9d3dc9
Daniel Steigerwald <daniel@steigerwald.cz>
2021-11-12 00:11:34 +0100

    Change WorkoutsList Pressable to Link

--- components/lists/WorkoutsList.tsx
+++ components/lists/WorkoutsList.tsx
@@ -1,13 +1,12 @@
 import { eq } from 'fp-ts';
 import { Eq } from 'fp-ts/Eq';
-import { useRouter } from 'next/router';
 import { memo } from 'react';
-import { Pressable, Text } from 'react-native';
+import { Text, View } from 'react-native';
 import stc from 'string-to-color';
 import { eqWorkout, Workout } from '../../codecs/domain';
-import { Route } from '../../codecs/routing';
 import { useAppState } from '../../contexts/AppStateContext';
 import { useTheme } from '../../contexts/ThemeContext';
+import { Link } from '../elements/Link';
 import { InsetBorder } from '../InsetBorder';
 
 interface WorkoutItemProps {
@@ -20,35 +19,22 @@ const eqWorkoutItemProps: Eq<{ workout: Workout }> = eq.struct({
 
 const WorkoutItem = memo<WorkoutItemProps>(({ workout }) => {
   const t = useTheme();
-  const router = useRouter();
 
-  const handlePressablePress = () => {
-    const route: Route = {
-      pathname: '/workout/[id]',
-      query: { id: workout.id },
-    };
-    router.push(route);
-  };
-
-  // TODO: Pressable should be Link, but we have to
-  // sync LocalStorage across tabs first. It's must.
   return (
-    <Pressable
-      style={[t.mvSm, t.p, t.maxWFull]}
-      accessibilityRole="button"
-      onPress={handlePressablePress}
-    >
-      <InsetBorder style={[t.shadow, { shadowColor: stc(workout.name) }]} />
-      <Text
-        // numberOfLines clips emojis because it adds overflow-y: hidden;
-        // I don't think we need it. Only wwwwwwwwwwwwwwwwwwwwwwww wraps.
-        // numberOfLines={1}
-        selectable={false}
-        style={[t.text, t.color]}
-      >
-        {workout.name}
-      </Text>
-    </Pressable>
+    <Link href={{ pathname: '/workout/[id]', query: { id: workout.id } }}>
+      <View style={[t.mvSm, t.p, t.maxWFull]}>
+        <InsetBorder style={[t.shadow, { shadowColor: stc(workout.name) }]} />
+        <Text
+          // numberOfLines clips emojis because it adds overflow-y: hidden;
+          // I'm pretty sure we don't need it. Only wwwwwwwwwwwwwwwwwwwwwwww wraps.
+          // numberOfLines={1}
+          selectable={false}
+          style={[t.text, t.color]}
+        >
+          {workout.name}
+        </Text>
+      </View>
+    </Link>
   );
 }, eqWorkoutItemProps.equals);
 
